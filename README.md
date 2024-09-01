76842f3759b5835158ad9eab6ab7266fa533cc32
jv24601 <108680838+jv24601@users.noreply.github.com>
2024-09-01 15:21:31 -0500

    prepare for cross origin service

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 An API with a static database to provide literary quotes. To update the quotes DB, edit the data in quotes.csv, then re-run the entire quotes_admin.ipynb file.
 
-Sources:0. https://www.datacamp.com/tutorial/introduction-fastapi-tutorial,https://fastapi.tiangolo.com/tutorial/
+Sources: 0. https://www.datacamp.com/tutorial/introduction-fastapi-tutorial,https://fastapi.tiangolo.com/tutorial/
+
+1. Allowing public GET requests https://sentry.io/answers/fastapi-error-no-access-control-allow-origin-header-is-present-on-the-requested-resource/
--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 from fastapi import FastAPI
 import sqlite3
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# You can add additional URLs to this list, for example, the frontend's production domain, or other frontends.
+allowed_origins = [
+    "http://localhost:3000",
+    "https://benwright.blog"
+]
+
+
+
 
 DB = "quotes.sqlite"
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,
+    allow_credentials=True,
+    allow_methods=["GET"],
+    allow_headers=["X-Requested-With", "Content-Type"],
+)
 
 def row_to_json(row):
     return {"_id":row[0],"author":row[1],"book":row[2],"quote":row[3]}
